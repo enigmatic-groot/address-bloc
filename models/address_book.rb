46679bf1bca5b79bb399ac63f7ed879d5e74601a
Jeremiah Judkins <jeremiah.a.judkins@gmail.com>
2018-08-08 20:28:03 -0700

    assignment-2-address-bloc

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,22 +1,27 @@
-class AddressBook
-  attr_reader :entries
+require_relative 'entry'
 
+class AddressBook
+  attr_accessor :entries
   def initialize
     @entries = []
   end
-end
-
-def add_entry(name, phone_number, email)
-    # #9
+  def add_entry(name, phone_number, email)
     index = 0
-    entries.each do |entry|
-    # #10
+    @entries.each do |entry|
       if name < entry.name
         break
       end
-      index+= 1
+      index += 1
     end
-    # #11
-    entries.insert(index, Entry.new(name, phone_number, email))
+    @entries.insert(index, Entry.new(name, phone_number, email))
+  end
+  def remove_entry(name, phone_number, email)
+    removed_entry = nil
+    @entries.each do |entry|
+      if name == entry.name && phone_number == entry.phone_number && email == entry.email
+        removed_entry = entry
+      end
+    end
+    @entries.delete(removed_entry)
   end
 end

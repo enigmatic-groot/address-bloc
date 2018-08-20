a72e90fe822b831e551b66042029c45ecac6bd42
Jeremiah Judkins <jeremiah.a.judkins@gmail.com>
2018-08-20 13:22:19 -0700

    checkpoint-5-address-bloc

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,20 +1,21 @@
+require_relative 'entry'
+
 class AddressBook
   attr_reader :entries
 
   def initialize
     @entries = []
   end
-end
 
-def add_entry(name, phone_number, email)
+  def add_entry(name, phone_number, email)
     # #9
     index = 0
     entries.each do |entry|
-    # #10
+      # #10
       if name < entry.name
         break
       end
-      index+= 1
+      index += 1
     end
     # #11
     entries.insert(index, Entry.new(name, phone_number, email))
